31091348ab70e4664a3e31ac8e78042db8ba4c5b
Elias <elikapeluschnik@gmail.com>
2021-11-24 10:15:22 -0500

    commit tree 9f99063954d156bc4a7cf966a0a1ea4ddbfaf98a

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,18 @@
+require 'pry'
+class Book
 
+    attr_accessor :title, :author, :page_count, :genre
 
+    def initialize(title)
+        @title = title
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+end
+
+# t1 = Book.new("And Then There Were None", "Agatha Christie")
+
+#binding.pry
